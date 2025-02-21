765562bfbb5776d1a02d5b62c62a6c247abb9b16
BrandenJoe <147530777+BrandenJoe@users.noreply.github.com>
2025-02-21 14:12:18 -0600

    adding nav

---
+++ src/components/nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function nav() {
+  return (
+    <nav>
+        <div className="nav_container">
+            <a href="/">
+            </a>
+        </div>
+    </nav>
+  )
+}

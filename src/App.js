2912c5f92242f51f51c91b620f153aae08748f46
BrandenJoe <147530777+BrandenJoe@users.noreply.github.com>
2025-02-22 14:01:42 -0600

    added languages I know and making a section for my challenges

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Nav from './components/nav'
 import Landing from './components/Landing';
+import Languages from './components/languages';
+import Challenges from './components/challenges';
 function App() {
   return (
     <div className="App">
     <Nav/>
     <Landing/>
+    <Languages/>
+    <Challenges/>
     </div>
   );
 }
---
+++ src/components/challenges.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function challenges() {
+  return (
+    <div>
+      <section id="projects">
+    <div class="container">
+      <div class="row">
+        <h1 class="section__title">Here are some of my <span class="text--purple">Challenges</span> I have completed</h1>
+        
+      </div>
+    </div>
+  </section>
+    </div>
+  )
+}
+
+export default challenges

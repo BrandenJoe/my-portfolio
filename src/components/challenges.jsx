2396ce8081d957a9f508214697baa5c1f35353c4
BrandenJoe <147530777+BrandenJoe@users.noreply.github.com>
2025-02-28 19:04:57 -0600

    adding my challenges

--- src/components/challenges.jsx
+++ src/components/challenges.jsx
@@ -4,10 +4,83 @@ function challenges() {
   return (
     <div>
       <section id="projects">
-    <div class="container">
-      <div class="row">
-        <h1 class="section__title">Here are some of my <span class="text--purple">Challenges</span> I have completed</h1>
-        
+    <div className="container">
+      <div className="row">
+        <h1 className="section__title">Here are some of my <span className="text--purple">Challenges</span> I have completed</h1>
+        <ul className="project__list">
+          <li className="project">
+            <div className="project__wrapper">
+              <img src='./assets/concurrency.png' className="project__img" alt="Concurrency project"/>
+              <div className="project__description">
+                <h3 className="project__description--title">Concurrency project</h3>
+                <h4 className="project__description--sub-title">
+                  Html, CSS, JavaScript, React
+                </h4>
+                <p className="project__description--para">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed diam leo, scelerisque sed odio eget,
+                  luctus faucibus augue. Fusce volutpat iaculis dapibus. Lorem ipsum dolor sit amet, consectetur
+                  adipiscing elit. Maecenas id eleifend libero. In hac habitasse platea dictumst.
+                </p>
+                <div className="project__description--links">
+                  <a href="https://www.github.com/yourlink" target="_blank" className="project__description--link">
+                    <i className="fab fa-github"></i>
+                  </a>
+                  <a href="https://www.yourwebsite.com" className="project__description--link">
+                    <i className="fas fa-link"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </li>
+          <li className="project">
+            <div className="project__wrapper">
+              <img src="./assets/loopit.png" className="project__img" alt="Car subscription project"/>
+              <div className="project__description">
+                <h3 className="project__description--title">Car sales project</h3>
+                <h4 className="project__description--sub-title">
+                  Html, CSS, JavaScript, Vue, Vuex
+                </h4>
+                <p className="project__description--para">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed diam leo, scelerisque sed odio eget,
+                  luctus faucibus augue. Fusce volutpat iaculis dapibus. Lorem ipsum dolor sit amet, consectetur
+                  adipiscing elit. Maecenas id eleifend libero. In hac habitasse platea dictumst.
+                </p>
+                <div className="project__description--links">
+                  <a href="https://www.github.com/yourlink" target="_blank" className="project__description--link">
+                    <i className="fab fa-github"></i>
+                  </a>
+                  <a href="https://www.yourwebsite.com" className="project__description--link">
+                    <i className="fas fa-link"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </li>
+          <li className="project">
+            <div className="project__wrapper">
+              <img src="./assets/loopit.png" className="project__img" alt="Car subscription project"/>
+              <div className="project__description">
+                <h3 className="project__description--title">Car sales project</h3>
+                <h4 className="project__description--sub-title">
+                  Html, CSS, JavaScript, Vue, Vuex
+                </h4>
+                <p className="project__description--para">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed diam leo, scelerisque sed odio eget,
+                  luctus faucibus augue. Fusce volutpat iaculis dapibus. Lorem ipsum dolor sit amet, consectetur
+                  adipiscing elit. Maecenas id eleifend libero. In hac habitasse platea dictumst.
+                </p>
+                <div className="project__description--links">
+                  <a href="https://www.github.com/yourlink" target="_blank" className="project__description--link">
+                    <i className="fab fa-github"></i>
+                  </a>
+                  <a href="https://www.yourwebsite.com" className="project__description--link">
+                    <i className="fas fa-link"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </li>
+        </ul>      
       </div>
     </div>
   </section>
--- src/components/languages.jsx
+++ src/components/languages.jsx
@@ -4,38 +4,38 @@ function languages() {
   return (
     <div>
   <section id="languages">
-    <div class="container">
-      <div class="row">
-        <h1 class="section__title">This is my <span class="text--purple">technology stack</span></h1>
-        <div class="language__list">
-          <div class="language">
-            <figure class="language__img--wrapper">
+    <div className="container">
+      <div className="row">
+        <h1 className="section__title">This is my <span className="text--purple">technology stack</span></h1>
+        <div className="language__list">
+          <div className="language">
+            <figure className="language__img--wrapper">
               <img
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/HTML5_Badge.svg/240px-HTML5_Badge.svg.png"
-                alt="Html Logo" class="language__img" />
+                alt="Html Logo" className="language__img" />
             </figure>
-            <span class="language__name">HTML</span>
+            <span className="language__name">HTML</span>
           </div>
-          <div class="language">
-            <figure class="language__img--wrapper">
+          <div className="language">
+            <figure className="language__img--wrapper">
               <img src="https://cdn.iconscout.com/icon/free/png-256/css-131-722685.png" alt="CSS Logo"
-                class="language__img" />
+                className="language__img" />
             </figure>
-            <span class="language__name">CSS</span>
+            <span className="language__name">CSS</span>
           </div>
-          <div class="language">
-            <figure class="language__img--wrapper">
+          <div className="language">
+            <figure className="language__img--wrapper">
               <img src="https://cdn.iconscout.com/icon/free/png-256/javascript-1-225993.png" alt="JavaScript Logo"
-                class="language__img" />
+                className="language__img" />
             </figure>
-            <span class="language__name">JavaScript</span>
+            <span className="language__name">JavaScript</span>
           </div>
-          <div class="language">
-            <figure class="language__img--wrapper">
+          <div className="language">
+            <figure className="language__img--wrapper">
               <img src="https://cdn.iconscout.com/icon/free/png-256/react-3-1175109.png" alt="React Logo"
-                class="language__img" />
+                className="language__img" />
             </figure>
-            <span class="language__name">React</span>
+            <span className="language__name">React</span>
           </div>
         </div>
       </div>

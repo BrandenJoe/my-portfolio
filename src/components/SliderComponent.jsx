0337e19bf10a90e82806ddb65eac09c4f7e87f3f
BrandenJoe <147530777+BrandenJoe@users.noreply.github.com>
2025-03-12 17:46:28 -0500

    trying to implement a Slider

---
+++ src/components/SliderComponent.jsx
@@ -0,0 +1,99 @@
+import React from 'react'
+import Slider from 'react-slick'
+import concurrency from '../assets/concurrency.png'
+import pomodoro from '../assets/pomodoro.PNG'
+import stopwatch from '../assets/stopwatch.png'
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+import './SliderComponent.css' // Import the CSS file
+
+
+function SliderComponent() {
+    const settings = {
+      className: "center",
+      centerMode: true,
+      infinite: true,
+      centerPadding: "60px",
+      slidesToShow: 3,
+      speed: 500
+    };
+
+  return (
+    <div className="slider-container">
+    <Slider {...settings}>
+      <div className="project">
+        <div className="project__wrapper">
+          <img src={concurrency} className="project__img" alt="Concurrency project"/>
+          <div className="project__description">
+            <h3 className="project__description--title">Concurrency project</h3>
+            <h4 className="project__description--sub-title">
+              Html, CSS, JavaScript, React
+            </h4>
+            <p className="project__description--para">
+              This project is about loading 100 user profiles in under 1.5 seconds and doing that is using Promise.all to call all of them at once.
+            </p>
+            <div className="project__description--links">
+              <a href="https://www.github.com/yourlink" target="_blank" className="project__description--link">
+                <i className="fab fa-github"></i>
+              </a>
+              <a href="https://www.yourwebsite.com" className="project__description--link">
+                <i className="fas fa-link"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="project">
+        <div className="project__wrapper">
+          <img src={pomodoro} className="project__img" alt="Pomodoro project"/>
+          <div className="project__description">
+            <h3 className="project__description--title">Pomodoro project</h3>
+            <h4 className="project__description--sub-title">
+              Html, CSS, JavaScript, Vue, Vuex
+            </h4>
+            <p className="project__description--para">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed diam leo, scelerisque sed odio eget,
+              luctus faucibus augue. Fusce volutpat iaculis dapibus. Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit. Maecenas id eleifend libero. In hac habitasse platea dictumst.
+            </p>
+            <div className="project__description--links">
+              <a href="https://www.github.com/yourlink" target="_blank" className="project__description--link">
+                <i className="fab fa-github"></i>
+              </a>
+              <a href="https://www.yourwebsite.com" className="project__description--link">
+                <i className="fas fa-link"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="project">
+        <div className="project__wrapper">
+          <img src={stopwatch} className="project__img" alt="Stopwatch project"/>
+          <div className="project__description">
+            <h3 className="project__description--title">Stopwatch project</h3>
+            <h4 className="project__description--sub-title">
+              Html, CSS, JavaScript, Vue, Vuex
+            </h4>
+            <p className="project__description--para">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed diam leo, scelerisque sed odio eget,
+              luctus faucibus augue. Fusce volutpat iaculis dapibus. Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit. Maecenas id eleifend libero. In hac habitasse platea dictumst.
+            </p>
+            <div className="project__description--links">
+              <a href="https://www.github.com/yourlink" target="_blank" className="project__description--link">
+                <i className="fab fa-github"></i>
+              </a>
+              <a href="https://www.yourwebsite.com" className="project__description--link">
+                <i className="fas fa-link"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Slider>
+  </div>
+  )
+}
+
+export default SliderComponent
--- src/components/challenges.jsx
+++ src/components/challenges.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import SliderComponent from './SliderComponent'
 import concurrency from '../assets/concurrency.png'
 import pomodoro from '../assets/pomodoro.PNG'
 import stopwatch from '../assets/stopwatch.png'
+
 function challenges() {
   return (
     <div>
@@ -9,78 +11,7 @@ function challenges() {
     <div className="container">
       <div className="row">
         <h1 className="section__title">Here are some of my <span className="text--purple">Challenges</span> I have completed</h1>
-        <ul className="project__list">
-          <li className="project">
-            <div className="project__wrapper">
-              <img src={concurrency} className="project__img" alt="Concurrency project"/>
-              <div className="project__description">
-                <h3 className="project__description--title">Concurrency project</h3>
-                <h4 className="project__description--sub-title">
-                  Html, CSS, JavaScript, React
-                </h4>
-                <p className="project__description--para">
-                 This project is about loading 100 user profiles in under 1.5 seconds and doing that is using Promise.all to call all of them at once.
-                </p>
-                <div className="project__description--links">
-                  <a href="https://www.github.com/yourlink" target="_blank" className="project__description--link">
-                    <i className="fab fa-github"></i>
-                  </a>
-                  <a href="https://www.yourwebsite.com" className="project__description--link">
-                    <i className="fas fa-link"></i>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </li>
-          <li className="project">
-            <div className="project__wrapper">
-              <img src={pomodoro} className="project__img" alt="Car subscription project"/>
-              <div className="project__description">
-                <h3 className="project__description--title">Car sales project</h3>
-                <h4 className="project__description--sub-title">
-                  Html, CSS, JavaScript, Vue, Vuex
-                </h4>
-                <p className="project__description--para">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed diam leo, scelerisque sed odio eget,
-                  luctus faucibus augue. Fusce volutpat iaculis dapibus. Lorem ipsum dolor sit amet, consectetur
-                  adipiscing elit. Maecenas id eleifend libero. In hac habitasse platea dictumst.
-                </p>
-                <div className="project__description--links">
-                  <a href="https://www.github.com/yourlink" target="_blank" className="project__description--link">
-                    <i className="fab fa-github"></i>
-                  </a>
-                  <a href="https://www.yourwebsite.com" className="project__description--link">
-                    <i className="fas fa-link"></i>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </li>
-          <li className="project">
-            <div className="project__wrapper">
-              <img src={stopwatch} className="project__img" alt="Car subscription project"/>
-              <div className="project__description">
-                <h3 className="project__description--title">Car sales project</h3>
-                <h4 className="project__description--sub-title">
-                  Html, CSS, JavaScript, Vue, Vuex
-                </h4>
-                <p className="project__description--para">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed diam leo, scelerisque sed odio eget,
-                  luctus faucibus augue. Fusce volutpat iaculis dapibus. Lorem ipsum dolor sit amet, consectetur
-                  adipiscing elit. Maecenas id eleifend libero. In hac habitasse platea dictumst.
-                </p>
-                <div className="project__description--links">
-                  <a href="https://www.github.com/yourlink" target="_blank" className="project__description--link">
-                    <i className="fab fa-github"></i>
-                  </a>
-                  <a href="https://www.yourwebsite.com" className="project__description--link">
-                    <i className="fas fa-link"></i>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </li>
-        </ul>      
+        <SliderComponent/>      
       </div>
     </div>
   </section>
